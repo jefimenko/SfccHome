b33d2eccb31cf5b4ef64428726314a4354935afd
Mat Lee <ml7@uw.edu>
2017-09-06 16:52:05 -0700

    Adding automation of static root creation.

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,35 @@
 # Usage:
 # ./deployment.sh [l]
 #
-# l command line arg will cause installation/packaging of libraries
+# 'l' command line arg will cause installation/packaging of libraries
 # specified in requirements-vendor.txt.
-python manage.py collectstatic
 
+# Assuming *nix like structure convention, and existing /var/www dir
+# *causes password prompt where applicable for sudo commands
+if [[ -d /var/www/sfcc ]]
+then
+    sudo mkdir /var/www/sfcc
+fi
+if [[ -d /var/www/sfcc/home ]]
+then
+    sudo mkdir /var/www/sfcc/home
+fi
+if [[ -d /var/www/sfcc/home/static ]]
+then
+    sudo mkdir /var/www/sfcc/home/static
+fi
+
+LOCAL_USER=$(whoami)
+sudo chown -R $LOCAL_USER:$LOCAL_USER /var/www/sfcc
+
+python manage.py collectstatic
 gsutil rsync -R /var/www/sfcc/home/static/ gs://sfcc-home-static/
+
 
 if [[ $1 == 'l' ]]
 then
     pip install --upgrade -r requirements-vendor.txt -t lib
 fi
+
 
 gcloud app deploy
