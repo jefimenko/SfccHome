935d92e87db083a7fbd7f052d00d9dd21192ad3f
Mat Lee <ml7@uw.edu>
2017-09-05 17:14:42 -0700

    Static files config.

--- SfccHome/settings.py
+++ SfccHome/settings.py
@@ -120,4 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+if DEBUG:
+    STATIC_URL = '/static/'
+else:
+    STATIC_URL = 'http://storage.googleapis.com/sfcc-home/static/'
+
+STATICFILES_DIRS = [
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
