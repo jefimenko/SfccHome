da9d5882cbfcada693c99fc8f8b0819f4c949e58
Mat Lee <ml7@uw.edu>
2017-09-05 17:14:18 -0700

    Moving/resetting secret key.

--- SfccHome/settings.py
+++ SfccHome/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+import settings_secrets
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kvs@r$d-b!pzle#qo6%_4eodv1l9_%uckp70_($v)yvlfkq-lz'
+SECRET_KEY = settings_secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
 ALLOWED_HOSTS = []
 
