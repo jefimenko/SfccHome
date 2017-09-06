63431839413e325c620b96bda0d7f09de9254ac8
Mat Lee <ml7@uw.edu>
2017-09-06 16:44:10 -0700

    Tweaking deploy to only install/package requirements-vendor.txt on demand.

--- deployment.sh
+++ deployment.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
+# Usage:
+# ./deployment.sh [l]
+#
+# l command line arg will cause installation/packaging of libraries
+# specified in requirements-vendor.txt.
 python manage.py collectstatic
 
-gsutil rsync -R static/ gs://sfcc-home-static/
+gsutil rsync -R /var/www/sfcc/home/static/ gs://sfcc-home-static/
 
-pip install --upgrade -r requirements-vendor.txt -t lib
+if [[ $1 == 'l' ]]
+then
+    pip install --upgrade -r requirements-vendor.txt -t lib
+fi
 
 gcloud app deploy
-
